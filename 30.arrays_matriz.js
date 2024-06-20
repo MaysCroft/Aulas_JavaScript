a0d3293b67a53a32438a48278f38bfc69c25ee7e
MaysCroft <mayconpilar19@gmail.com>
2024-06-19 21:46:36 -0300

    Aulas JavaScript 19/06/2024

---
+++ 30.arrays_matriz.js
@@ -0,0 +1,16 @@
+// Utilizando Arrays Bidimensionais - MATRIZ
+
+let numeros = [
+    [14, 22, 45, 17],
+    [11, 33, 76, 32],
+    [43, 21, 19, 20],
+    [26, 13, 98, 37],
+];
+
+console.table(numeros);     // Usa o termo "table" para VIZUALIZAR o Array em uma TABELA
+
+console.log(numeros[2][2]); // Usa dois [][] para identificar LINHA e COLUNA da Tabela
+
+numeros [3][1] = 41;        // Usa para ALTERAR uma parte específica do Array
+
+console.table(numeros);
